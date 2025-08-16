5a6849d52800cba1d2bb3cd67e4a7477717061bf
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2025-08-16 21:07:18 +0600

    fix admin cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,9 @@
-from typing import Any
 from flask import Flask
-from flask_login import LoginManager
-from flask_limiter.util import get_remote_address
 from flask_cors import CORS
 
 from config import get_config
 
 from app.admin import register_crud_blueprints
-from app.models.admin_user import AdminUser
 from app.core.db import db
 from app.core.jwt import init_jwt
 from app.api.routes import api_bp
@@ -24,19 +20,15 @@ def create_app() -> Flask:
     db.init_app(app)
     register_crud_blueprints(app)
 
-    login_manager: LoginManager = LoginManager()
-    login_manager.init_app(app)
-    # login_manager.login_view = "admin_auth.admin_login"
-
-    @login_manager.user_loader
-    def load_user(user_id) -> Any | None:
-        return AdminUser.query.get(int(user_id))
-
     app.register_blueprint(api_bp, url_prefix="/api")
     app.register_blueprint(admin_api_bp, url_prefix="/admin")
 
     init_jwt(app)
-    CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"},  r"/admin/*": {"origins": "*"}})
-    CORS(app, origins=["http://localhost:5173"], supports_credentials=True)
+    CORS(
+        app,
+        origins=["*"],
+        supports_credentials=True,
+        resources={r"/*": {"origins": "*"}},
+    )
 
     return app
